3ecd031992eff31009bf4b6756e7b806bf4dfebb
mesneym <mesney.mesney@gmail.com>
2020-05-04 13:36:00 -0400

    Added plot

--- code/plot1.py
+++ code/plot1.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 
 fig,ax = plt.subplots()
 x,y = np.loadtxt('resultcv.csv', delimiter=',', unpack=True)
-x,y = np.loadtxt('result.csv', delimiter=',', unpack=True)
+x2,y2 = np.loadtxt('result.csv', delimiter=',', unpack=True)
 
-for i in range(len(x)):
-    ax.plot(x[i],y[i],'o')
-    plt.pause(0.01)
+ax.plot(x,y,'o')
+ax.plot(x2,y2,'o')
+
+plt.show()
+# for i in range(len(x)):
+    # ax.plot(x[i],y[i],'o')
+    # plt.pause(0.01)
 
 
 
